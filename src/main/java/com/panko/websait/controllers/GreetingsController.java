106ed3341f40d6f23a5e08105bb749b6b58478cc
Artiom <pontiara96@mail.ru>
2020-09-18 23:38:19 +0300

    Added using mustache and additionally added first Controller

--- src/main/java/com/panko/websait/controllers/GreetingsController.java
+++ src/main/java/com/panko/websait/controllers/GreetingsController.java
@@ -2,13 +2,22 @@ package com.panko.websait.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 @Controller
 public class GreetingsController {
 
-    @GetMapping
-    public String greeting() {
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(required = false) String name, Map<String, Object> model) {
 
         return "greeting";
+    }
+
+    @GetMapping
+    public String main(Map<String, Object> model) {
+
+        return "main";
     }
 }
