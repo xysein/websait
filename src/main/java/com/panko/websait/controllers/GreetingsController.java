0ccba1c1217daac4a7cf09555056aefeb5bd2d15
Artiom <pontiara96@mail.ru>
2020-09-20 00:54:20 +0300

    Added using mustache and additionally added first Controller

--- src/main/java/com/panko/websait/controllers/GreetingsController.java
+++ src/main/java/com/panko/websait/controllers/GreetingsController.java
@@ -1,13 +1,22 @@
 package com.panko.websait.controllers;
 
+import com.panko.websait.models.Message;
+import com.panko.websait.repositories.MessageRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
 @Controller
 public class GreetingsController {
+
+    private final MessageRepository messageRepository;
+
+    public GreetingsController(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(required = false) String name, Map<String, Object> model) {
@@ -17,6 +26,32 @@ public class GreetingsController {
 
     @GetMapping
     public String main(Map<String, Object> model) {
+        model.put("messages", messageRepository.findAll());
+
+        return "main";
+    }
+
+    @PostMapping
+    public String add(@RequestParam String text, @RequestParam String tag, @RequestParam Map<String, Object> model) {
+        messageRepository.save(new Message(text, tag));
+
+        Iterable<Message> messages = messageRepository.findAll();
+        model.put("messages", messages);
+
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
+        Iterable<Message> messages;
+
+        if (filter == null || filter.isEmpty()) {
+            messages = messageRepository.findAll();
+        } else {
+            messages = messageRepository.findByTag(filter);
+        }
+
+        model.put("messages", messages);
 
         return "main";
     }
---
+++ src/main/java/com/panko/websait/repositories/MessageRepository.java
@@ -0,0 +1,10 @@
+package com.panko.websait.repositories;
+
+import com.panko.websait.models.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepository extends CrudRepository<Message, Long> {
+    List<Message> findByTag(String tag);
+}
